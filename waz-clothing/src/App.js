d896018947465b343fda4c0800fe2944551d68be
NicolasTassin <nicolastassin598@gmail.com>
2020-01-26 08:55:11 +0100

    google auth

--- waz-clothing/src/App.js
+++ waz-clothing/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 
 
@@ -12,9 +14,11 @@ import ShopPage from './pages/shop/shop.component'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndSignUpPage}/>
       </Switch>
      
     </div>
